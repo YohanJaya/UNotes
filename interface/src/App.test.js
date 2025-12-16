ab99f6b271ad1d1ee0663f99fa68d0ec5af1d9d3
YohanJaya <nyajayasinghedmc10@gmail.com>
2025-12-16 09:28:45 +0530

    Fix frontend failing jobs(4)

--- interface/src/App.test.js
+++ interface/src/App.test.js
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 test('renders UNotes heading', () => {
   render(<App />);
-  expect(screen.getByRole('heading', { name:  /# unotes/i })).toBeInTheDocument();
+  const heading = screen.getByRole('heading', { name: /unotes/i });
+  expect(heading).toBeInTheDocument();
 });
